a0d747cf879cc75bd90aece13ffa8ff36ad82d52
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-09-02 11:54:25 -0300

    main

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () =>{
+  return(
+    <Header className="header">
+      Header
+    </Header>
+  );
+
+};
+
+export default Header;
